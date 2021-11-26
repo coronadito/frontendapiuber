0c32d49b2c3df2d8e9728d95ebd4a3805e46548c
coronadito <alejandra.coronadojl@gmail.com>
2021-11-25 21:01:09 -0500

    estructura base del proyecto/modulo iniciosesion

---
+++ src/app/modulos/seguridad/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import * as cryptoJS from 'crypto-js'; 
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  fgValidacion = this.fb.group({
+    correo: ['', [Validators.required, Validators.email]],
+    clave: ['', [Validators.required]]
+  });
+
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+
+  identificarUsuario() {}
+}
